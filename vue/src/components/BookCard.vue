3c7c5fa958c092806c8ee6c9d517d07300af72bf
Hibo Jama <Hibojama717@gmail.com>
2022-12-09 09:01:20 -0500

    pushing

--- vue/src/components/BookCard.vue
+++ vue/src/components/BookCard.vue
@@ -33,8 +33,8 @@ export default {
 
 .badge {
     position: absolute;
-left:21px;
-top:7px;    
+    left:21px;
+    top:7px;    
     text-transform: uppercase;
     font-size: 13px;
     font-weight: 700;
@@ -42,69 +42,20 @@ top:7px;
     color: #fff;
     padding: 3px 20px;
 }
-
-/* 
-.returningValue{
-    flex: 1;
-    margin: 3px;
-    height: 70vh;
-    position: relative;
-}
-
-/* .badge {
-    position: absolute;
-    left:138px;
-    top:33px;
-    text-transform: uppercase;
-    font-size: 13px;
-    font-weight: 700;
-    background: rgba(3, 3, 243, 0.795);
-    color: #fff;
-    padding: 3px 20px;
-} */
 
 .book-title {
     margin-bottom: 20px;
 }
 
-/* .book-author {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-} */
 
-/* .CoverImg {
-    /* width: 100%;
-    height: 100%; */
-    /* object-fit: cover; */
-    /* display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    height: 75%;
-}  */ 
 
 .returningValue  { 
     width: 95%; 
     font: inherit;
-    /* margin-top:1.5rem; */
     position:relative;
     width: 399px; 
     height: 511px;
     max-width: 350px; 
-    /* object-fit: cover;
-    flex: 1;
-     display: flex; 
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center; */
     float:left; 
     display: inline;
     white-space: inherit;
@@ -126,14 +77,9 @@ top:7px;
 
 .book-title {
  position:relative;
-/* left:38px;
-top:405px; */
 left:38px;
 top:408px;
-/* max-width: 500px; */
-/* width:275px; */
-/* white-space: initial; */
-/* word-wrap:break-word; */
+
 } 
 
  .book-author {
@@ -152,17 +98,6 @@ top:-121px;
 height: 400px;
 }
 
-/* .book-title {
-left:57px;
-top:410px;
-position:relative;
-}
-
-.book-author {
-left:91px;
-top:398px;
-background-color:#ffffff;
-} */
 
 
 
