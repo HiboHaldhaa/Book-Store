cb12727b761be27236336f364b295822c31bf158
Edgardo Martinez <eddy.mtez@gmail.com>
2022-12-10 17:16:23 -0500

    added send book via email to book cards

--- vue/src/components/BookCard.vue
+++ vue/src/components/BookCard.vue
@@ -4,14 +4,42 @@
     <h2 class="book-title">{{book.title}} </h2>
     <h3 class="book-author"> {{book.author}} </h3>
     <img class="CoverImg " v-bind:src="book.coverLink">
+    <div class="shareBtn">
+    <button class="shareButton" v-on:click="showShareForm()">Share</button>
+    </div>
+    <form id="shareForm" @submit.prevent="sendemail()">
+    <div>
+      <label for="email">Email: </label>
+      <input id="email" type="text" v-model.trim="email" placeholder="Recipient email"/>
+    </div>
+    <div>
+      <label for="name">From: </label>
+      <input id="name" type="text" v-model.trim="name" placeholder="Your Name"/>
+    </div>
+    <div><button>Send Book</button></div>
+    
+   
+
+  </form>
+
 <div v-if="isNew" class="badge">New</div>
+
   </div>
   <!-- </div> -->
 </template>
 <script>
+import EmailService from '../services/EmailService'
+
 export default {
   name : 'book-card',
   props : ['book'],
+  data() {
+    return {
+      email: "",
+      name: ""
+    }
+    
+  },
   computed: {
     isNew() {
       const today = new Date();
@@ -22,10 +50,27 @@ export default {
       }
       return false;
   }
+},
+methods: {
+  showShareForm() {
+    document.getElementById('shareForm').style.display = "block";
+  },
+  sendemail() {
+
+    EmailService.sendEmail(this.email, this.name, this.book);
+
+    document.getElementById('shareForm').style.display = "none";
+
+  }
 }
 }
 </script>
 <style>
+.shareBtn {
+  position: absolute;
+  top: 350px;
+  left: 40px;
+}
 .badge {
     position: absolute;
     left:21px;
@@ -77,5 +122,14 @@ position:relative;
 left:21px;
 top:-121px;
 height: 400px;
+}
+
+#shareForm {
+  position: absolute;
+  bottom: 100px;
+  left: 100px;
+  z-index: 999;
+  display: none;
+  background-color: white;
 }
 </style>
