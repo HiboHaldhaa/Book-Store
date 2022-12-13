2c73f19e01e3b00b9b6fff6f593900b4ea5fbce6
Edgardo Martinez <eddy.mtez@gmail.com>
2022-12-12 22:37:20 -0500

    Added google Book API to search results

--- vue/src/components/SearchResults.vue
+++ vue/src/components/SearchResults.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
-      <BookCard v-bind:book="book" v-for="book in booklist" v-bind:key="book.isbn"/>
-
+    <BookCard v-bind:book="book" v-for="book in booklist" v-bind:key="book.isbn"/>
+    <div id="apiBooks">
+        <ApiBookSearch v-if="booklist.length > 0" />
+    </div>
   </div>    
 </template>
 
 <script>
 import BookCard from '../components/BookCard.vue';
+import ApiBookSearch from './ApiBookSearch.vue';
 export default {
 components: {
-    BookCard
-
+    BookCard,
+    ApiBookSearch
 },
 
 computed: {
     booklist() {
         return this.$store.state.currentSearch;
-    },
+    }
 },
 mounted() {
     this.$store.commit('SET_CURRENT_SEARCH', []);
